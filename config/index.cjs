c0a69d63937c7f5016efbf16b3897d66ae966b67
y1whiwh <124333499+y1whiwh@users.noreply.github.com>
2023-02-03 16:27:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,10 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f913a3a24fcb60c57a20eacef0e93d01',
   
-  // 模板消息id
-  TENPLATE_ID:'O6N3ngR10AtwgZUhO9Ltj7-hfEiiNQCjHS9wP4wXwAA',
-  
-  USERS:{'oSgXn5k35HXu28-OJ-Q1qyiv3Mik'},
+ 
 
   PROVINCE: '黑龙江',
   CITY: '哈尔滨',
@@ -26,9 +23,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '姜春旭',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oSgXn5k35HXu28-OJ-Q1qyiv3Mik',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'O6N3ngR10AtwgZUhO9Ltj7-hfEiiNQCjHS9wP4wXwAA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '2-15',
       festivals: [
